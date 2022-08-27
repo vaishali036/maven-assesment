a85739ab42247300cc5b02827447a9ced0fdfd63
Divyaraj-Mandale <111412336+Divyaraj-Mandale@users.noreply.github.com>
2022-08-27 17:46:24 +0530

    Update LocationRestController.java

--- src/main/java/com/locationweb/controller/LocationRestController.java
+++ src/main/java/com/locationweb/controller/LocationRestController.java
@@ -11,22 +11,84 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.locationweb.entities.Locations;
 import com.locationweb.repositories.LocationRepository;
+import java.util.List;
 
-@RestController
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.locationweb.dto.LocationData;
+import com.locationweb.entities.Locations;
+import com.locationweb.services.LocationService;
+import com.locationweb.utility.EmailService;
+
+@Controller
 @RequestMapping("/location/api")
 public class LocationRestController {
 
 	@Autowired
 	private LocationRepository locationRepo;
 	
+	@Autowired
+	private LocationService locationService;
+	
+	@Autowired
+	private EmailService emailService;
+	
+	//handler method
+	
 	@RequestMapping("show")
 	public String showLocationPage() {
 		return "create_location"; 
 	}
+	@RequestMapping("/saveLocation")
+	public String saveLocation(@ModelAttribute("loc") Locations location, ModelMap model) {
+		locationService.savaLocation(location);
+		//emailService.sendSimpleMessage("rajmandale18@gmail.com", "Test", "welcome msg");
+		//model.addAttribute("msg", "LOCATION IS SAVED...!");
+		model.addAttribute("msg", "LOCATION IS Saved...!");
+		return "create_location";
+	}
+	
+	@RequestMapping("listall")
+	public String listAll(ModelMap model) {
+		List<Locations> locations = locationService.getAllLocations();
+		model.addAttribute("locations" , locations);
+		return "search_Result";
+	}
+	@RequestMapping("/delete")
+	public String deleteLocation(@RequestParam("id") long id,ModelMap model) {
+		locationService.deleteLocationById(id);
+		List<Locations> locations = locationService.getAllLocations();
+		model.addAttribute("locations" , locations);
+		return "search_Result";
+	}
+	
+	@RequestMapping("/update")
+	public String updateLocation(@RequestParam("id") long id,ModelMap model) {
+		Locations location = locationService.getLocationById(id);
+		model.addAttribute("location" , location);
+		return "update_location";
+	}
+	
+	@RequestMapping("/updateData")
+	public String updateLocation(@ModelAttribute("loc") Locations location, ModelMap model) {
+		locationService.savaLocation(location);
+		model.addAttribute("msg", "LOCATION IS updated...!");
+		return "update_location";
+	}
+	
+	
+	
+	
+	
 	
 	@GetMapping
 	public List<Locations> getLocations(){
